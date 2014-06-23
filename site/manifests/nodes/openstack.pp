c60a6616f7f3491996a87c65dcbc6150e14c2807
uberj <jaxbr503@gmail.com>
2014-06-23 09:14:13 -0700

    add other roles

--- site/manifests/nodes/openstack.pp
+++ site/manifests/nodes/openstack.pp
@@ -2,3 +2,14 @@ node 'refspec2.vm.labs.scl3.mozilla.com' {
   include ::openstack::role::controller
 }
 
+node 'refspec3.vm.labs.scl3.mozilla.com' {
+  include ::openstack::role::network
+}
+
+node 'refspec4.vm.labs.scl3.mozilla.com' {
+  include ::openstack::role::compute
+}
+
+node 'refspec5.vm.labs.scl3.mozilla.com' {
+  include ::openstack::role::compute
+}
