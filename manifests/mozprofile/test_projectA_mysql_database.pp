8f60a30cc6de972647c9bb51aa550724a86e75d2
uberj <jaxbr503@gmail.com>
2014-07-14 17:43:17 -0700

    try a name change

--- manifests/mozprofile/test_projectA_mysql_database.pp
+++ manifests/mozprofile/test_projectA_mysql_database.pp
@@ -1,4 +1,4 @@
-class mozprofile::test_projectA_mysql_database
+class mozpuppet::mozprofile::test_projectA_mysql_database
 { 
   class { '::profile::mysql_database':
     root_password    => heira('projectA::mysql_password'),
