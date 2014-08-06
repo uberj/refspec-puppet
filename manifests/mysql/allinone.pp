9a18ea9ebed757b8215d1d9c767a39a77519fc0d
uberj <jaxbr503@gmail.com>
2014-08-06 11:25:12 -0700

    add mysql bem and fem

---
+++ manifests/mysql/allinone.pp
@@ -0,0 +1,24 @@
+class ::m::mysql::allinone (
+  $root_password,
+  $user,
+  $password,
+  $allowed_hosts,
+) {
+  class { '::mysql::server':
+    root_password     => $::root_password,
+    restart           => true,
+    override_options  => {
+      'mysqld' => {
+        'bind-address' => '0.0.0.0',
+      }
+    }
+  }
+
+  ::mysql::db { $::db_name:
+    user     => $::username,
+    password => $::password,
+    host     => $::allowed_hosts,
+    grant    => ['ALL']
+  }
+}
+
---
+++ manifests/mysql/python_client.pp
@@ -0,0 +1,8 @@
+class ::m::mysql::python_client () {
+  include mysql::client
+
+  class { 'mysql::bindings':
+    python_enable => true
+  }
+
+}
